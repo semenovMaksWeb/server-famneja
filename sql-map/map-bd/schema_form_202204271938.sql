23a52839b6cc95c4c8201e9c6103be258e04b054
Maks <semenov@mail.ru>
2022-04-27 19:38:59 +0400

    map-schema-form

---
+++ sql-map/map-bd/schema_form_202204271938.sql
@@ -0,0 +1,28 @@
+INSERT INTO public.schema_form (id,id_form,id_components,id_parent) VALUES
+	 (2,4,21,NULL),
+	 (3,4,22,21),
+	 (4,4,23,21),
+	 (5,4,24,21),
+	 (6,6,25,NULL),
+	 (7,6,26,25),
+	 (8,6,27,25),
+	 (9,6,28,25),
+	 (10,14,29,NULL),
+	 (11,14,30,29);
+INSERT INTO public.schema_form (id,id_form,id_components,id_parent) VALUES
+	 (12,14,31,29),
+	 (13,14,32,29),
+	 (14,15,33,NULL),
+	 (15,15,34,33),
+	 (16,15,35,33),
+	 (17,15,36,33),
+	 (18,18,37,NULL),
+	 (19,18,38,37),
+	 (20,18,39,38),
+	 (21,18,40,38);
+INSERT INTO public.schema_form (id,id_form,id_components,id_parent) VALUES
+	 (22,18,41,37),
+	 (23,18,42,41),
+	 (24,18,43,42),
+	 (25,18,44,42),
+	 (26,18,45,42);
