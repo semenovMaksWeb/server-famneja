8ff20b97ef93bc03813f5f4a268bd5faa4108710
Maks <semenov@mail.ru>
2022-05-09 11:49:27 +0400

    full fun code

--- sql-fun/main.sql
+++ sql-fun/main.sql
@@ -29,7 +29,7 @@ AS $function$
 $function$
 ;
 
-CREATE OR REPLACE FUNCTION components.component_get_name()
+CREATE OR REPLACE FUNCTION component_get_name(_name varchar)
  RETURNS TABLE(component_id integer, component_name varchar)
  LANGUAGE plpgsql
 AS $function$
@@ -38,7 +38,8 @@ AS $function$
           select
             c.id as component_id,
             c.name as component_name
-            from components."component" c;
+            from components."component" c
+            where c.name = _name;
 	END;
 $function$
 ;
