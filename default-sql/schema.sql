a8f1a9040fbd18762c44b663d4333485f7c2b96b
Semenov Maks <Semenov@mail.ru>
2022-05-22 16:31:06 +0400

    fix schema

--- default-sql/schema.sql
+++ default-sql/schema.sql
@@ -203,14 +203,14 @@ CREATE TABLE components.component (
 
 -- DROP TABLE components.component_example;
 
-CREATE TABLE components.component_example (
+ CREATE TABLE components.component_example (
 	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	id_component int4 NOT NULL,
 	"class" json NULL,
 	"style" json NULL,
 	id_parent int4 NULL,
 	id_rights int4 NULL,
-	tag varchar NULL,
+	order int4 NULL,
 	CONSTRAINT component_example_pk PRIMARY KEY (id),
 	CONSTRAINT component_example_fk FOREIGN KEY (id_component) REFERENCES components.component(id),
 	CONSTRAINT component_example_fk_2 FOREIGN KEY (id_parent) REFERENCES components.component_example(id),
